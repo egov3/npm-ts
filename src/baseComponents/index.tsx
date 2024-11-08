db927c8362d462be70620c5585bdb3685ec5bb5a
Zhassulan Baigozha <zhassulan.baigozha@nitec.kz>
2024-11-08 13:14:19 +0500

    fix: [egov-0806] refactor

---
+++ src/baseComponents/index.tsx
@@ -0,0 +1,5 @@
+import { InputField } from "./InputField";
+
+export const baseComponents = {
+  InputField,
+};
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { baseComponents } from "./baseComponents";
+
+export const add = (a: number, b: number): number =>{
+  return a + b;
+}
+
+export default {
+  baseComponents,
+  add
+} 
